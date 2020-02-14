a91ec0462fbc52834a18ba3bb50ec1966972d617
Marcus <marcusvmleite@gmail.com>
2020-02-14 11:15:08 +0100

    collectors samples

---
+++ src/main/java/com/marcusvmleite/parallelstreams/BasicParallelSample.java
@@ -0,0 +1,42 @@
+package com.marcusvmleite.parallelstreams;
+
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+public class BasicParallelSample {
+
+    static {
+        //Manually setting the thread pool size used by parallelism.
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "12");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(iterateOver(5));
+        System.out.println(iterateOverParallel(5));
+    }
+
+    private static long iterateOver(int n) {
+        return Stream.iterate(1, i -> i + 1)
+                .limit(n)
+                .reduce(0, Integer::sum);
+    }
+
+    //ATTENTION!
+    //iterate() is not a good approach for parallelism because the numbers have
+    //to be unboxed before being added, what causes an overhead.
+    //In that case the better choice is to use specialized streams!
+    private static long iterateOverParallel(int n) {
+        return Stream.iterate(1, i -> i + 1)
+                .limit(n)
+                .parallel()
+                .reduce(0, Integer::sum);
+    }
+
+    //rangeClosed() uses primitive types.
+    private static long betterApproach(long n) {
+        return LongStream.rangeClosed(1, n)
+                .parallel()
+                .reduce(0, Long::sum);
+    }
+
+}
